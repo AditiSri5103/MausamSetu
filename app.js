b5ac3a989d8ab970906f1365b450584116f20520
Aditi Srivastava <aditisrivastava7788@gmail.com>
2025-06-18 01:43:57 +0530

    Farmer Registeration module completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const path = require("path")
 require("dotenv").config();
+const fs = require("fs")
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,6 +15,34 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 
 });
+app.get("/register", function (req, res) {
+    res.sendFile(__dirname + "/register.html")
+});
+
+app.post("/register-farmer", function (req, res) {
+    console.log(req);
+    const farmer_name = req.body.name;
+    const farmer_city = req.body.city;
+    const farmer_contact = req.body.contact;
+    let farmers = []
+
+    try {
+        if (fs.existsSync("farmers-info.json")) {
+            const data = fs.readFileSync("farmers-info.json", "utf-8");
+            farmers = JSON.parse(data);
+        }
+    } catch (err) {
+        console.error("Error reading farmers.json:", err);
+    }
+    farmers.push({ farmer_name, farmer_city, farmer_contact })
+    fs.writeFile("farmers-info.json", JSON.stringify(farmers, null, 2), (err) => {
+        if (err) {
+            console.error("Error saving farmer:", err);
+            return res.send("Registration failed. Try again.");
+        }
+        res.send("Farmer registered successfully. <a href='/'>Go to Home</a>");
+    });
+});
 
 app.post("/", function (req, res) {
     const query = req.body.city;
@@ -22,15 +51,15 @@ app.post("/", function (req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=metric";
 
     https.get(url, function (response) {
-        console.log(response.statusCode);    
+        console.log(response.statusCode);
         response.on("data", function (data) {
             try {
                 const weatherData = JSON.parse(data);
-    
+
                 if (+weatherData.cod !== 200) {
-                    return res.json({error:"City not found"});
+                    return res.json({ error: "City not found" });
                 }
-    
+
                 const temp = weatherData.main.temp;
                 const humidity = weatherData.main.humidity;
                 const desc = weatherData.weather[0].description;
@@ -38,15 +67,15 @@ app.post("/", function (req, res) {
                 const iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 
                 res.json({
-                    city:query,
-                    temperature:temp,
-                    humid:humidity,
-                    description:desc,
-                    iconUrl:iconUrl
+                    city: query,
+                    temperature: temp,
+                    humid: humidity,
+                    description: desc,
+                    iconUrl: iconUrl
                 });
             } catch (error) {
                 console.error("Error parsing data:", error.message);
-                res.json({error:"Something went wrong. Please try again."});
+                res.json({ error: "Something went wrong. Please try again." });
             }
         });
     });
